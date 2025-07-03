8ff2c8d5f6490aec55fe4d712b76d6316403e043
chrish <chris@chrish.no>
2025-07-03 12:32:13 +0200

    ADded aws secret integration

--- maria.php
+++ maria.php
@@ -21,7 +21,7 @@ final class MariaDbConnection
         $port     = 3389;
         $dbname   = 'proddata';
         $username = 'root';
-        $password = 'Mb!5J7p2wSrx#D';   
+        $password = '12345678';   
 
         $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4"; // MariaDB uses the mysql PDO driver
 
